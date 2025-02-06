0b4498e63c4328d72068217824f6b35da190ccd0
Lalit-mahata <lm3654725@gmail.com>
2025-02-06 17:13:26 +0545

    aboutus and rest

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider} from "react-router-dom"
+import AboutUs from './components/AboutUs';
+import ResturantsMenu from './pages/ResturantsMenu';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route path='/about' element={<AboutUs/>}/>
+      <Route path='/menu' element={<ResturantsMenu/>}/>
+
+    </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 

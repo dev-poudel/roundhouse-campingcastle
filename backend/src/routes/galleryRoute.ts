e50bb901c2b73b597e729d7b69c94a9a82e6aaf7
Lalit-mahata <lm3654725@gmail.com>
2025-02-19 16:45:07 +0545

    change

--- backend/src/routes/galleryRoute.ts
+++ backend/src/routes/galleryRoute.ts
@@ -3,8 +3,8 @@ import { createGallery, deleteGallery, getAllGallery, getGalleryById, updateGall
 import { isAdmin, isAuthenticate } from "../middleware/authMiddleware";
 
 const router:Router = express.Router()
-router.route("/").post(createGallery) // Create Gallery
-router.route("/").get( getAllGallery); // Get All Gallery
+router.route("/").post(isAuthenticate,isAdmin,createGallery) // Create Gallery
+router.route("/").get(getAllGallery); // Get All Gallery
 router.route("/:id").get(getGalleryById); // Get Single Gallery
 router.route("/:id").put(isAuthenticate,isAdmin,updateGallery) // Update Gallery
 router.route("/:id").delete(isAuthenticate,isAdmin,deleteGallery); // Delete Gallery

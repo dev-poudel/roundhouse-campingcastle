42bdf4f00ce60bc32627f7a267b28ffda5d0a82b
Lalit-mahata <lm3654725@gmail.com>
2025-02-19 16:44:50 +0545

    change

--- backend/src/controller/galleryController.ts
+++ backend/src/controller/galleryController.ts
@@ -8,7 +8,7 @@ import { eq } from "drizzle-orm";
 export const createGallery = asyncHandler(async(req:Request,res:Response)=>{
 try {
     const {image,viewType} = req.body;
-    if ( !image ) {
+    if (!image ) {
           return res.status(400).json({ error: "Please fill all the fields" });
         }
     

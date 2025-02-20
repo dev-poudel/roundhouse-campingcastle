3d7ce82512e98e0a124f8ad3c81bd49ef3181642
Lalit-mahata <lm3654725@gmail.com>
2025-02-20 15:44:39 +0545

    changes

--- backend/src/drizzle/schema/rating.ts
+++ backend/src/drizzle/schema/rating.ts
@@ -1,6 +1,5 @@
 import { integer, pgTable, text } from "drizzle-orm/pg-core";
 import { id } from "../schemaHelper";
-import { UserTable } from "./user";
 
 export const RatingTable = pgTable("ratings",{
     id,
--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -38,7 +38,6 @@ export const isAuthenticate = asyncHandler(async (req: CustomRequest, res: Respo
 });
 
 export const isAdmin = asyncHandler(async (req: CustomRequest, res: Response, next: NextFunction) => {
-  
   const decoded = jwt.verify(req.body.jwt, process.env.JWT_SECRET as string) as DecodedToken;
   const [user] = await db.select().from(UserTable).where(eq(UserTable.id, decoded.userId));
   if (user.isAdmin) {

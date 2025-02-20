46137ae194d396e29279c12ac5d618b7bd410a11
Lalit-mahata <lm3654725@gmail.com>
2025-02-20 14:11:59 +0545

    updated

--- backend/src/controller/bookController.ts
+++ backend/src/controller/bookController.ts
@@ -48,7 +48,6 @@ export const getAllBookings = asyncHandler(async(req: Request, res: Response) =>
       },
       room: {
         booked : RoomTable.booked,
-        bookingExpired : RoomTable.bookingExpired,
         price : RoomTable.price,
         images : RoomTable.images
       },
@@ -80,7 +79,6 @@ export const getBookingById = asyncHandler(async(req: Request, res: Response) =>
       },
       room: {
         booked : RoomTable.booked,
-        bookingExpired : RoomTable.bookingExpired,
         price : RoomTable.price,
         images : RoomTable.images
       },
